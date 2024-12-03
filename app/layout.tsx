5b27652e44e26dc349df21e2491c88f0bbe480ec
Muhammad Azam <azam@polyphasicdevs.com>
2024-12-03 19:47:43 +0500

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const open = localFont({
   weight: "100 200 300 400 500 600 700 800 900",
 });
 const dmsans = localFont({
-  src: "./fonts/dmsans.ttf",
+  src: "../fonts/dmsans.ttf",
   variable: "--font-dmsans",
   weight: "100 200 300 400 500 600 700 800 900",
 });
